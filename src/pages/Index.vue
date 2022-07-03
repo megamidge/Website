a5957ea320300b61bf22d61fa1cdbd6701ee8cf3
Sam Smith <ss.samsmith98@outlook.com>
2022-07-03 13:37:46 +0100

    hide about

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,22 +3,28 @@
     <welcome id="welcome" />
     <home-portfolio id="portfolio" />
     <home-c-v id="cv" />
-    <home-about id="about" />
+    <!-- <home-about id="about" /> -->
     <home-contact id="contact" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import Welcome from "components/Welcome";
-import HomePortfolio from "src/components/Home-Portfolio.vue";
-import HomeCV from "src/components/Home-CV.vue";
-import HomeAbout from "src/components/Home-About.vue";
-import HomeContact from "src/components/Home-Contact.vue";
-import { debounce } from "quasar";
+import { defineComponent } from 'vue'
+import Welcome from 'components/Welcome'
+import HomePortfolio from 'src/components/Home-Portfolio.vue'
+import HomeCV from 'src/components/Home-CV.vue'
+// import HomeAbout from 'src/components/Home-About.vue'
+import HomeContact from 'src/components/Home-Contact.vue'
+import { debounce } from 'quasar'
 
 export default defineComponent({
-  name: "Home",
-  components: { Welcome, HomePortfolio, HomeCV, HomeAbout, HomeContact },
-});
+  name: 'Home',
+  components: {
+    Welcome,
+    HomePortfolio,
+    HomeCV,
+    //HomeAbout,
+    HomeContact,
+  },
+})
 </script>
